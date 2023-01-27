c767ef6cd862474c0021e68b6fbaad9a55c8963a
Nicolas Buzzi <nico.cbuzzi@gmail.com>
2023-01-27 14:02:14 -0300

    Updating/fixing integration tests

--- src/controllers/rest/HelloWorldController.integration.spec.ts
+++ src/controllers/rest/HelloWorldController.integration.spec.ts
@@ -10,7 +10,7 @@ describe("HelloWorldController", () => {
   beforeEach(
     PlatformTest.bootstrap(Server, {
       mount: {
-        "/": HelloWorldController
+        "/": [HelloWorldController]
       }
     })
   );
