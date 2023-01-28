a01aeeb663b2384223cb49fd83cdfabb5330a045
Nicolas Buzzi <nico.cbuzzi@gmail.com>
2023-01-27 21:34:33 -0300

    Upppppddaa

--- src/Server.ts
+++ src/Server.ts
@@ -11,7 +11,7 @@ import * as pages from "./controllers/pages/index";
 @Configuration({
   ...config,
   acceptMimes: ["application/json"],
-  httpPort: process.env.PORT || 8083,
+  httpPort: process.env.PORT || 5000,
   httpsPort: false, // CHANGE
   componentsScan: false,
   mount: {
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-// import { $log, PlatformBuilder } from "@tsed/common";
 import { PlatformExpress } from "@tsed/platform-express";
 import { Server } from "./Server";
 
 const platform = PlatformExpress.create(Server);
 platform.listen();
 
-// async function bootstrap() {
-//   try {
-//     var platforme = await PlatformExpress.bootstrap(Server);
-//     platform.app.use(express.static("public"));
-//     await platform.listen();
-
-//     process.on("SIGINT", () => {
-//       platform?.stop();
-//     });
-//   } catch (error) {
-//     $log.error({ event: "SERVER_BOOTSTRAP_ERROR", message: error.message, stack: error.stack });
-//   }
-// }
-
-// bootstrap();
-console.log("Server started");
 module.exports = platform.app;
