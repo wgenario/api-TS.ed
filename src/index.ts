471dd9f02cfab698b7611e6408df0ef4149350e7
Nicolas Buzzi <nico.cbuzzi@gmail.com>
2023-01-27 21:00:05 -0300

    Updating

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-import {$log} from "@tsed/common";
+import { $log, PlatformBuilder } from "@tsed/common";
 import { PlatformExpress } from "@tsed/platform-express";
-import {Server} from "./Server";
+import { Application } from "express-serve-static-core";
+import { Server } from "./Server";
+
+let platform: PlatformBuilder<Application> | undefined;
 
 async function bootstrap() {
   try {
-    const platform = await PlatformExpress.bootstrap(Server);
+    platform = await PlatformExpress.bootstrap(Server);
     await platform.listen();
 
     process.on("SIGINT", () => {
-      platform.stop();
+      platform?.stop();
     });
   } catch (error) {
-    $log.error({event: "SERVER_BOOTSTRAP_ERROR", message: error.message, stack: error.stack});
+    $log.error({ event: "SERVER_BOOTSTRAP_ERROR", message: error.message, stack: error.stack });
   }
 }
 
 bootstrap();
+
+module.exports = platform;
