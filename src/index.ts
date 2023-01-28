be150696e17da64ff8f93e3df4a7906d5029fe80
Nicolas Buzzi <nico.cbuzzi@gmail.com>
2023-01-27 21:25:54 -0300

    Updatiiing

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import { $log, PlatformBuilder } from "@tsed/common";
+// import { $log, PlatformBuilder } from "@tsed/common";
 import { PlatformExpress } from "@tsed/platform-express";
-import { Application } from "express-serve-static-core";
 import { Server } from "./Server";
 
-let platform: PlatformBuilder<Application> | undefined;
+const platform = PlatformExpress.create(Server);
+platform.listen();
 
-async function bootstrap() {
-  try {
-    platform = await PlatformExpress.bootstrap(Server);
-    await platform.listen();
+// async function bootstrap() {
+//   try {
+//     var platforme = await PlatformExpress.bootstrap(Server);
+//     platform.app.use(express.static("public"));
+//     await platform.listen();
 
-    process.on("SIGINT", () => {
-      platform?.stop();
-    });
-  } catch (error) {
-    $log.error({ event: "SERVER_BOOTSTRAP_ERROR", message: error.message, stack: error.stack });
-  }
-}
+//     process.on("SIGINT", () => {
+//       platform?.stop();
+//     });
+//   } catch (error) {
+//     $log.error({ event: "SERVER_BOOTSTRAP_ERROR", message: error.message, stack: error.stack });
+//   }
+// }
 
-bootstrap();
-
-module.exports = platform;
+// bootstrap();
+console.log("Server started");
+module.exports = platform.app;
